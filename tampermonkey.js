e8cd7fbd9fa882a228843d2c261bb6ff92d44a94
Mark Senff <mark@senff.com>
2019-06-16 10:04:50 -0400

    Create tampermonkey.js

---
+++ tampermonkey.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Hide Support Session indicators
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Makes your support session Calypso screen look normal for 10 seconds, for screenshot purposes
+// @author       Senff
+// @require      https://code.jquery.com/jquery-1.12.4.js
+// @match        https://wordpress.com/*
+// @grant        none
+// ==/UserScript==
+
+var $ = window.jQuery;
+
+function addNormalLookButton() {
+    if ($('body #wpcom > .is-support-session').length) {
+        $('.environment-badge').after('<div id="support-screen-shot">S</div>');
+    }
+}
+
+function removeStuff() {
+    $('#wpcom > .layout').removeClass('is-support-session');
+    $('.masterbar__quick-language-switcher, body > div:last-child, .environment-badge, #support-screen-shot').fadeOut(200);
+    setTimeout(function () {
+        $('#wpcom > .layout').addClass('is-support-session');
+        $('.masterbar__quick-language-switcher, body > div:last-child, .environment-badge, #support-screen-shot').fadeIn(1000);
+    }, 10000);
+}
+
+$(document).ready(function() {
+    addNormalLookButton();
+});
+
+$('body').on('click', '#support-screen-shot', function() {
+   removeStuff();
+});
